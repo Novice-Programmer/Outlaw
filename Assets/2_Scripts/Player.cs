3606acd6f7710a8027ca6451afc894679b6ded42
Novice-Programmer <57133119+Novice-Programmer@users.noreply.github.com>
2020-06-17 14:47:52 +0900

    Update

---
+++ Assets/2_Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : UnitBase
+{
+
+    CharacterController _controller;
+    [SerializeField] float _moveSpeed = 15;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        float z = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Vertical");
+
+        transform.Translate((Vector3.forward * x + Vector3.right * z) * Time.deltaTime * _moveSpeed);
+    }
+}
