7d4edd8dabcd28003648ba9ec4a0c1deb3df1bd2
Novice-Programmer <57133119+Novice-Programmer@users.noreply.github.com>
2020-06-17 17:48:10 +0900

    Update

--- Assets/2_Scripts/Player.cs
+++ Assets/2_Scripts/Player.cs
@@ -4,21 +4,93 @@ using UnityEngine;
 
 public class Player : UnitBase
 {
-
+    Animator _animator;
     CharacterController _controller;
-    [SerializeField] float _moveSpeed = 15;
+    GameObject _modelObj;
+    [SerializeField] float _runSpeed = 5;
+    [SerializeField] float _walkSpeed = 1.0f;
+    [SerializeField] float _sideSpeed = 0.2f;
 
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _controller = GetComponent<CharacterController>();
+        _modelObj = transform.GetChild(0).gameObject;
+        _animator.SetBool("IsBattle", true);
     }
 
 
     void Update()
     {
-        float z = Input.GetAxis("Horizontal");
-        float x = Input.GetAxis("Vertical");
+        float speed = _runSpeed;
+        float mz = Input.GetAxis("Horizontal");
+        float mx = Input.GetAxis("Vertical");
 
-        transform.Translate((Vector3.forward * x + Vector3.right * z) * Time.deltaTime * _moveSpeed);
+        Vector3 mv = new Vector3(mx, 0, -mz);
+        mv = (mv.magnitude > 1) ? mv.normalized : mv;
+
+        if (mv.magnitude == 0)
+            _animator.SetInteger("AniType", (int)eAniType.IDLE);
+        else if (mv.magnitude > 0)
+        {
+            _animator.SetInteger("AniType", (int)eAniType.RUN);
+            _modelObj.transform.rotation = Quaternion.LookRotation(mv);
+        }
+
+        // 방향을 받아서 방향에 따른 애니메이션 변화
+        //float speed = ChangeAnimationToDirection(mv);
+
+        _controller.Move(mv * speed * Time.deltaTime);
+    }
+
+    float ChangeAnimationToDirection(Vector3 dir)
+    {
+        float speed = _walkSpeed;
+        eAniType aniType = eAniType.IDLE;
+        if (dir == Vector3.zero)
+            aniType = eAniType.IDLE;
+        else
+        {
+            if (dir.z == 0)
+            {
+                if (dir.x > 0)
+                {
+                    speed = _sideSpeed;
+                    aniType = eAniType.WALK_LEFT;
+                }
+                else if (dir.x < 0)
+                {
+                    speed = _sideSpeed;
+                    aniType = eAniType.WALK_RIGHT;
+                }
+            }
+            else if (dir.z > 0)
+            {
+                speed = _runSpeed;
+                if (dir.x > 0)
+                {
+                    speed -= _sideSpeed;
+                }
+                else if (dir.x < 0)
+                {
+                    speed -= _sideSpeed;
+                }
+                aniType = eAniType.RUN;
+            }
+            else
+            {
+                aniType = eAniType.WALK_BACK;
+                if (dir.x > 0)
+                {
+                    speed -= _sideSpeed;
+                }
+                else if (dir.x < 0)
+                {
+                    speed -= _sideSpeed;
+                }
+            }
+        }
+        _animator.SetInteger("AniType", (int)aniType);
+        return speed;
     }
 }
--- Assets/2_Scripts/UnitBase.cs
+++ Assets/2_Scripts/UnitBase.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public class UnitBase : MonoBehaviour
 {
+    public enum eAniType
+    {
+        IDLE = 0,
+        RUN,
+        WALK_BACK,
+        WALK_LEFT,
+        WALK_RIGHT,
+        ATTACK,
+        RELOAD,
+        DEAD
+    }
 }
