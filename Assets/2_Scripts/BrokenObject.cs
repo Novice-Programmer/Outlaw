62d9f6cf048a2649fd3fc65a80495fdbc11c6e4c
Novice-Programmer <57133119+Novice-Programmer@users.noreply.github.com>
2020-06-21 23:54:59 +0900

    Update

--- Assets/2_Scripts/BrokenObject.cs
+++ Assets/2_Scripts/BrokenObject.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections;
+﻿using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Internal;
 
 public class BrokenObject : MonoBehaviour
 {
     [SerializeField] int _duration = 999;
+    [SerializeField] string _hitEffectName = "Hit";
+    [SerializeField] string _boomEffectName = "Explosion";
+    [SerializeField] string _frameEffectName = "Frame";
+    [SerializeField] int _effSize = 1;
     GameObject _effectHit;
     GameObject _effectExplosion;
     GameObject _effectFrame;
 
+    const string effPass = "Prefabs/ParticleEffects/";
+
     // Start is called before the first frame update
     void Awake()
     {
-        _effectHit = Resources.Load("Prefabs/ParticleEffects/Hit") as GameObject;
-        _effectExplosion = Resources.Load("Prefabs/ParticleEffects/Explosion") as GameObject;
-        _effectFrame = Resources.Load("Prefabs/ParticleEffects/Frame") as GameObject;
+        string hitPass = effPass + _hitEffectName;
+        string boomPass = effPass + _boomEffectName;
+        string framePass = effPass + _frameEffectName;
+
+        _effectHit = Resources.Load(hitPass) as GameObject;
+        _effectExplosion = Resources.Load(boomPass) as GameObject;
+        _effectFrame = Resources.Load(framePass) as GameObject;
     }
 
     // Update is called once per frame
